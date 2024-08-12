ed0a7a36fe6a3ea0346fe0df10b1d79779b39e63
Julianne Adams <julianne@julianneadams.dev>
2024-08-12 16:56:55 -0400

    Add: Bubble Tea

--- main.go
+++ main.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
+type model struct {
+	quitting bool
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch keypress := msg.String(); keypress {
+		case "q", "ctrl+c":
+			m.quitting = true
+			return m, tea.Quit
+		}
+	}
+
+	var cmd tea.Cmd
+	return m, cmd
+}
+
+func (m model) View() string {
+	if m.quitting {
+		return "Quitting...\n"
+	}
+	return "Hello World!\n"
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	m := model{quitting: false}
+
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
 }
