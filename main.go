5f8febdf9f77fc5cb74db3ba1e03ffda71f7d24c
Julianne Adams <julianne@julianneadams.dev>
2024-08-13 15:05:17 -0400

    Add: display tasks as a list

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 
+	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
@@ -13,8 +15,40 @@ type task struct {
 }
 
 type model struct {
+	list     list.Model
 	tasks    []task
 	quitting bool
+}
+
+type listItem string
+
+type listItemDelegate struct{}
+
+func (d listItemDelegate) Height() int                             { return 1 }
+func (d listItemDelegate) Spacing() int                            { return 0 }
+func (d listItemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d listItemDelegate) Render(w io.Writer, m list.Model, index int, item list.Item) {
+	i, ok := item.(listItem)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s", index+1, i)
+
+	fmt.Fprint(w, str)
+}
+
+func (i listItem) FilterValue() string { return "" }
+
+func (t task) toListItem() listItem {
+	doneStr := " "
+
+	if t.done {
+		doneStr = "x"
+	}
+	s := fmt.Sprintf("[%s] %s", doneStr, t.description)
+
+	return listItem(s)
 }
 
 func (m model) Init() tea.Cmd {
@@ -40,19 +74,7 @@ func (m model) View() string {
 		return "Quitting...\n"
 	}
 
-	output := ""
-
-	for _, task := range m.tasks {
-		var doneStr string
-		if task.done {
-			doneStr = "x"
-		} else {
-			doneStr = " "
-		}
-
-		output += fmt.Sprintf("[%s] %s\n", doneStr, task.description)
-	}
-	return output
+	return m.list.View()
 }
 
 func main() {
@@ -60,7 +82,14 @@ func main() {
 		{description: "Example", done: false},
 		{description: "Example 2", done: true},
 	}
-	m := model{quitting: false, tasks: tasks}
+
+	items := []list.Item{
+		tasks[0].toListItem(),
+		tasks[1].toListItem(),
+	}
+
+	l := list.New(items, listItemDelegate{}, 20, 14)
+	m := model{quitting: false, tasks: tasks, list: l}
 
 	if _, err := tea.NewProgram(m).Run(); err != nil {
 		fmt.Println("Error running program:", err)
